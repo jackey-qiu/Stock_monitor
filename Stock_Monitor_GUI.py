77957e6ea9d29497acad0754db2e8d2da594f92e
Canrong Qiu <canrong@Canrongs-iMac.home>
2021-03-23 22:03:38 +0100

    functionalities for displaying index of different types finished!

--- Stock_Monitor_GUI.py
+++ Stock_Monitor_GUI.py
@@ -27,6 +27,7 @@ from scipy import signal
 # import scipy.signal.savgol_filter as savgol_filter
 import pyqtgraph as pg
 from pyqtgraph import QtCore, QtGui
+import datetime
 
 #from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
 def error_pop_up(msg_text = 'error', window_title = ['Error','Information','Warning'][0]):
@@ -58,8 +59,9 @@ class CandlestickItem(pg.GraphicsObject):
         self.picture = QtGui.QPicture()
         p = QtGui.QPainter(self.picture)
         p.setPen(pg.mkPen('w'))
-        w = (self.data[1][0] - self.data[0][0]) / 3.
-        for (t, open, close, min, max) in self.data:
+        w = (self.data.iloc[1,0] - self.data.iloc[0,0]) / 3.
+        for i in range(len(self.data)):
+            (t, open, close, min, max,*_) = self.data.iloc[i]
             p.drawLine(QtCore.QPointF(t, min), QtCore.QPointF(t, max))
             if open > close:
                 p.setBrush(pg.mkBrush('g'))
@@ -83,27 +85,97 @@ class MyMainWindow(QMainWindow):
         uic.loadUi(os.path.join(script_path,'stock_monitor_app.ui'),self)
         # self.setupUi(self)
         # plt.style.use('ggplot')
-        # self.widget_terminal.update_name_space('main_gui',self)
+        self.widget_terminal.update_name_space('main_gui',self)
         # self.addToolBar(self.mplwidget.navi_toolbar)
-        self.setWindowTitle('Chniese Stock Monitor')
-        self.pushButton_plot_dapan_within.clicked.connect(lambda:self.plot_dapan_profile(code = '000001', start = '2021-03-10', end = '2021-03-22'))
+        self.setWindowTitle('Chinese Stock Monitor')
+        self.pushButton_extract_1.clicked.connect(lambda:self.plot_dapan_profile(code = data_extractor.index_code_map[self.comboBox_index_type.currentText()], start = (datetime.datetime.today()-datetime.timedelta(365*self.spinBox_previous_years.value())).strftime('%Y-%m-%d'), end = datetime.datetime.today().strftime('%Y-%m-%d')))
+        self.pushButton_exract_2.clicked.connect(lambda:self.plot_dapan_profile(code = data_extractor.index_code_map[self.comboBox_index_type.currentText()], start = self.lineEdit_begin_date.text(), end = self.lineEdit_end_date.text()))
+        self.comboBox_index_type.currentTextChanged.connect(self.extract_selected_index)
+        self.pushButton_last_week.clicked.connect(lambda:self.plot_dapan_profile(code = data_extractor.index_code_map[self.comboBox_index_type.currentText()], start = (datetime.datetime.today()-datetime.timedelta(7)).strftime('%Y-%m-%d'), end = datetime.datetime.today().strftime('%Y-%m-%d')))
+        self.pushButton_last_month.clicked.connect(lambda:self.plot_dapan_profile(code = data_extractor.index_code_map[self.comboBox_index_type.currentText()], start = (datetime.datetime.today()-datetime.timedelta(30)).strftime('%Y-%m-%d'), end = datetime.datetime.today().strftime('%Y-%m-%d')))
+        self.pushButton_last_three_month.clicked.connect(lambda:self.plot_dapan_profile(code = data_extractor.index_code_map[self.comboBox_index_type.currentText()], start = (datetime.datetime.today()-datetime.timedelta(90)).strftime('%Y-%m-%d'), end = datetime.datetime.today().strftime('%Y-%m-%d')))
+        self.pushButton_last_six_month.clicked.connect(lambda:self.plot_dapan_profile(code = data_extractor.index_code_map[self.comboBox_index_type.currentText()], start = (datetime.datetime.today()-datetime.timedelta(183)).strftime('%Y-%m-%d'), end = datetime.datetime.today().strftime('%Y-%m-%d')))
+        self.pushButton_last_year.clicked.connect(lambda:self.plot_dapan_profile(code = data_extractor.index_code_map[self.comboBox_index_type.currentText()], start = (datetime.datetime.today()-datetime.timedelta(365)).strftime('%Y-%m-%d'), end = datetime.datetime.today().strftime('%Y-%m-%d')))
+        self.values_000001 = []
+        self.values_000016 = []
+        self.values_000300 = []
+        self.values_000688 = []
+        self.values_specified = []
+        self.extract_selected_index()
+        self.setup_plot()
+
+    def extract_selected_index(self):
+        code = data_extractor.index_code_map[self.comboBox_index_type.currentText()]
+        if len(getattr(self,'values_{}'.format(code)))==0:
+            setattr(self,'values_{}'.format(code),data_extractor.extract_all_records(code))
+        self.set_current_price(code)
+
+    def setup_plot(self):
+        self.lr = pg.LinearRegionItem()
+        self.lr.setZValue(-10)
+        self.ax_dapan = self.mplwidget_dapan.addPlot(clear = True)
+        self.ax_dapan_zoomin = self.mplwidget_dapan.addPlot(clear = True)
+        self.lr.sigRegionChanged.connect(self._updatePlot)
+        self.ax_dapan_zoomin.sigXRangeChanged.connect(self._updateRegion)
+        self.ax_dapan.addItem(self.lr)
+        self.vLine = pg.InfiniteLine(angle=90, movable=False)
+        self.hLine = pg.InfiniteLine(angle=0, movable=False)
+        self.ax_dapan_zoomin.addItem(self.vLine, ignoreBounds=True)
+        self.ax_dapan_zoomin.addItem(self.hLine, ignoreBounds=True)
+        self.label = pg.LabelItem(justify='right')
+        # self.ax_dapan_zoomin.addItem(self.label)
+        self.proxy = pg.SignalProxy(self.ax_dapan_zoomin.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
+
+
+    def set_current_price(self, code):
+        if len(getattr(self,'values_{}'.format(code)))==0:
+            setattr(self,'values_{}'.format(code),data_extractor.extract_all_records(code))
+        self.lineEdit_close_price.setText(str(getattr(self,'values_{}'.format(code))['close_price'].iloc[-1]))
+        self.lineEdit_open_price.setText(str(getattr(self,'values_{}'.format(code))['open_price'].iloc[-1]))
+        self.lineEdit_high_price.setText(str(getattr(self,'values_{}'.format(code))['high_price'].iloc[-1]))
+        self.lineEdit_low_price.setText(str(getattr(self,'values_{}'.format(code))['low_price'].iloc[-1]))
+        self.lineEdit_volume.setText(str(getattr(self,'values_{}'.format(code))['total'].iloc[-1]))
+        self.lineEdit_amount.setText(str(getattr(self,'values_{}'.format(code))['trancaction'].iloc[-1]))
+        self.lineEdit_change_rate.setText('%'+str(getattr(self,'values_{}'.format(code))['rate'].iloc[-1]))
+
+    def mouseMoved(self,evt):
+        pos = evt[0]  ## using signal proxy turns original arguments into a tuple
+        if self.ax_dapan_zoomin.sceneBoundingRect().contains(pos):
+            mousePoint = self.ax_dapan_zoomin.vb.mapSceneToView(pos)
+            index = int(mousePoint.x())
+            x = (self.values_specified['date']-index).apply(abs).values.argmin() 
+            results = [self.values_specified[each].iloc[x] for each in ['date','open_price','close_price','high_price','low_price','rate','total','trancaction']]
+            results[0] = (datetime.date(1, 1, 1)+datetime.timedelta(int(results[0]))).strftime('%Y-%m-%d')
+            self.lineEdit_single_point.setText('开盘日期:{};    开盘价:{};    收盘价:{};    最高值:{};    最低值:{};    涨跌幅:{}%;    总市值:{};    成交量:{}'.format(*results))
+            # self.label.setText("<span style='font-size: 12pt'>x=%0.1f,   <span style='color: red'>y1=%0.1f</span>,   <span style='color: green'>y2=%0.1f</span>" % (mousePoint.x(), 1, 2))
+            self.vLine.setPos(mousePoint.x())
+            self.hLine.setPos(mousePoint.y())
+
+    def _updatePlot(self):
+        self.ax_dapan_zoomin.setXRange(*self.lr.getRegion(), padding=0)
+
+    def _updateRegion(self):
+        self.lr.setRegion(self.ax_dapan_zoomin.getViewBox().viewRange()[0])
 
     def plot_dapan_profile(self, code = '000001', start = '2019-01-01', end = '2021-03-22'):
-        # self.mplwidget_dapan.canvas.figure.clear()
-        values = data_extractor.extract_index_data(code, start, end)
+        self.mplwidget_dapan.clear()
+        self.setup_plot()
+        if getattr(self,'values_{}'.format(code)).__len__() == 0:                
+            setattr(self,'values_{}'.format(code),data_extractor.extract_all_records(code))
+        values = data_extractor.extract_index_data(code, start, end, getattr(self,'values_{}'.format(code)))
+        self.values_specified = values
         item = CandlestickItem(values)
-        ax = self.mplwidget_dapan.addPlot()
-        ax.addItem(item)
-
-        #ax.plot(values[:,0])
-        # fplt.candlestick_ochl(values, ax=ax)
-        # fplt.show()
-        #mpf.candlestick_ohlc(ax,values,width=1.2,colorup='r',colordown='green')
-        # self.mplwidget_dapan.canvas.draw()
+        item2 = CandlestickItem(values)
+        self.ax_dapan.addItem(item)
+        self.ax_dapan_zoomin.addItem(item2)
+        ticks = [(each, (datetime.date(1, 1, 1)+datetime.timedelta(each)).strftime('%y-%m-%d')) for each in values['date']]
+        self.ax_dapan.getAxis('bottom').setTicks([ticks])
+        self.ax_dapan_zoomin.getAxis('bottom').setTicks([ticks])
+        # self._updatePlot()
 
 
 if __name__ == "__main__":
-    QApplication.setStyle("windows")
+    QApplication.setStyle("fusion")
     app = QApplication(sys.argv)
     myWin = MyMainWindow()
     # app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
--- data_extractor.py
+++ data_extractor.py
@@ -4,18 +4,28 @@ import matplotlib.pyplot as plt
 import urllib.request as urllib2
 import json
 import datetime
+from io import StringIO
 
-index_code_map = {'shangzheng':'000001','shangzheng50':'000016'}
+index_code_map = {'上证指数':'000001','上证50':'000016','沪深300':'000300','科创50':'000688'}
 url_template = 'http://push2his.eastmoney.com/api/qt/stock/kline/get?cb=jQuery1124034703156772714716_1606741623783&secid=1.{}&ut=fa5fd1943c7b386f172d6893dbfba10b&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58&klt=101&fqt=0&beg=19900101&end=20220101&_=1606741623987'
 
-def extract_index_data(code, start, end):
+def extract_all_records(code):
     Req=urllib2.Request(url_template.format(code))
     Respon=urllib2.urlopen(Req)
     Res = Respon.read().decode()
     Res=Res[Res.find('klines')+8:len(Res)-4]
     Res_json = json.loads(Res)
-    data_in_target = []
+    results = pd.read_csv(StringIO('\n'.join(Res_json)),names = ['date','open_price','close_price','high_price','low_price','trancaction','total','rate'])
+    results['date'] = results['date'].apply(pd.Timestamp)
+    return results
+
+def extract_index_data(code, start, end, results = []):
+    if len(results)==0:
+        results = extract_all_records(code)
+    data_in_target = results[(results['date']<=pd.Timestamp(end)) & (results['date']>=pd.Timestamp(start))]
+    data_in_target['date'] = data_in_target['date'].apply(lambda x: (x.date()-datetime.date(1, 1, 1)).days)
     #dates_in_target = []
+    '''
     start = datetime.datetime.strptime(start,'%Y-%m-%d')
     end = datetime.datetime.strptime(end,'%Y-%m-%d')
     date_0001_01_01 = datetime.date(1, 1, 1)
@@ -36,7 +46,8 @@ def extract_index_data(code, start, end):
                 break
             else:
                 pass
-    return data_in_target
+    '''
+    return data_in_target[['date','open_price','close_price','low_price','high_price','trancaction','total','rate']]
         
 
     
